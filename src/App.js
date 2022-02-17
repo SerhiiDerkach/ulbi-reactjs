9121ec48de3b1a8d98d84bf58d06ecbdcd9959f3
siko1 <serhiy.siko@gmail.com>
2022-02-17 22:43:30 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useRef, useState } from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import "./styles/App.css";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 function App() {
+
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript1', body: 'Description'},
+    {id: 2, title: 'Javascript 2', body: 'Description'},
+    {id: 3, title: 'Javascript 3', body: 'Description'}
+  ])
+
+  const [title, setTitle] = useState('')
+  const bodyInputRef = useRef();
+  const addNewPost = (e) => {
+    e.preventDefault()
+    console.log(title);
+    console.log(bodyInputRef.current.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        {/* Керований компонент */}
+        <MyInput 
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text" 
+          placeholder="Назва поста"
+        />
+        {/* Некерований компонент */}
+        <MyInput 
+          ref={bodyInputRef}
+          type="text" 
+          placeholder="Опис поста"
+        />
+        <MyButton onClick={addNewPost}>Створити пост</MyButton>
+      </form>
+      <PostList posts={posts} title= "Пости про JS"/>
     </div>
   );
 }
